858593215ecf6945e618b7d11d80eb143b7cc713
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-02-13 14:06:31 +0200

    lots of changes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,56 @@
-(function($) {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
+$('.navbar-toggle').click('click', function(e) {
+    $('.navbar-collapse').css(
+        "background-color","lightgray").toggle()
     $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
+             $('.navbar-toggle:visible').click();
+     });
+});
+$('a.page-scroll').click('click', function(e) {
+    var $anchor = $(this);
+         $('html, body').stop().animate({
+             scrollTop: ($($anchor.attr('href')).offset().top+3)
+         }, 1250, 'easeInOutExpo');
+         event.preventDefault();
+})
+var aChildren = $(".navbar-right li").children(); // find the a children of the list items
+var aArray = []; // create the empty aArray
+ $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("nav-active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("nav-active");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("nav li:last-child a").addClass("nav-active");
+            }
+        }
     });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+// (function($) {
+//     $('a.page-scroll').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: ($($anchor.attr('href')).offset().top - 50)
+//         }, 1250, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
 
-});
+//     // Highlight the top nav as scrolling occurs
+//     $('body').scrollspy({
+//         target: '.navbar-fixed-top',
+//         offset: 51
+//     });
 
