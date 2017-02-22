4699484386792219d91f5b3d0a3d16ccc7bd1bd1
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-02-22 12:24:59 +0200

    added guessthenumber and articles.json

---
+++ js/guessthenumber.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('#submit-question').click(guessTheNumber);
+});
+var randomInteger = getRandomInteger(0,10);
+function getRandomInteger( min, max ) {
+	return Math.floor((Math.random() * max) + 1);
+};
+function compareNumbers( first, second ) {
+
+	if(first == second) {
+		alert("You did it! Well done! Now you can try again");
+		$('#submit-question').prop('disabled',false);
+	}
+
+	else {
+		alert("Nope, not that one...");
+		$('#submit-question').prop('disabled',false);
+	}
+}
+function guessTheNumber() {
+
+	var guessedNumber = $('#question').val();
+	if(guessedNumber >= 0 && $('#question').val() <= 10) {
+		$('#submit-question').prop('disabled',true);
+		compareNumbers(guessedNumber,randomInteger);
+		randomInteger = getRandomInteger(0,10);
+	}
+
+	else {
+		alert("You fool, you didn't enter a valid number!!! Try again");
+		guessedNumber = '';
+		$('#submit-question').prop('disabled',false);
+	}
+}
