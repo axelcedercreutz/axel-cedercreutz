a6ca5021cd24f0b605c47c3b48dae88bd400e3e9
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-03-01 11:54:21 +0200

    fixed some shit

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,29 +1,20 @@
 $(document).ready(function(){
 	var loop;
-	var options = {
-		url: 'https://project-8145367811882739054.firebaseio.com/.json',
-		type: 'GET',
-		dataType:'jsonp'
-	}
 	var articles = [];
-	$.ajax(options)
-			
-    	.then(function(res){
-    		//success response
-    		//console.log(res);
-    		addArticle(res.articles);
-    	},function(err){
-    		//error response
-    		console.log(err);
-    	})
-        .then(function() {
-            $(window).unload(saveSettings);
-            loadSettings();
-        });
+    $.getJSON('https://project-8145367811882739054.firebaseio.com/.json',function(res){
+        $(window).unload(saveSettings);
+        loadSettings();
+        addArticle(res.articles)
+    })
 });
+//array for the articles
 var htmlTexts = [];
+//number of articles that come back
 var articleAmount = 0;
+//Id for the article
 var article_index = -1;
+// Function that adds the article to the correct div and starts the loop
+// with the returned articles
 function addArticle(data) {
 	for (var i = 0; i < data.length; i++) {
 		articleAmount = data.length;
@@ -35,21 +26,41 @@ function addArticle(data) {
 	}
 	loopArticles(htmlTexts);
 }
+//function that fades in a article with a specific index
 function showArticle(index) {
 	$('#newsArticle-'+index).fadeIn(2000);
 }
+// function that loops the articles. Starts witb hiding the 
+// last seen article, then defines what the next index should be
+// and then fades in the next article
+
+//count 
+
+var count = 0
 function loopArticles() {
-	loop = setInterval(function(){
+    if(count === 0) {
+        if(article_index == articleAmount - 1) {
+            article_index = 0;
+        }
+        else {
+            article_index ++;
+        }
+        $('#newsArticle-'+article_index).fadeIn(2000);
+        count ++;
+    };
+    // console.log(count);
+    loop = setInterval(function(){
 		$('#newsArticle-' + (article_index)).hide();
-		if(article_index == articleAmount - 1) {
-		 	article_index = 0;
-		}
-		else {
-			article_index ++;
-		}
-		$('#newsArticle-'+article_index).fadeIn(2000);
+        if(article_index == articleAmount - 1) {
+            article_index = 0;
+        }
+        else {
+            article_index ++;
+        }
+        $('#newsArticle-'+article_index).fadeIn(2000);
 	},3000);
 };
+//
 $('#start_stop').click('click', function(e) {
     var $this = $(this);
     $this.toggleClass('Play');
@@ -62,6 +73,7 @@ $('#start_stop').click('click', function(e) {
         $this.text("Stop");
     }
 });
+
 $('#next').click('click',function(e) {
     var $this = $(this);
     clearInterval(loop);
@@ -76,6 +88,7 @@ $('#next').click('click',function(e) {
     }
     $('#newsArticle-'+article_index).fadeIn(2000);
 })
+
 $('#previous').click('click',function(e) {
     var $this = $(this);
     clearInterval(loop);
@@ -90,8 +103,8 @@ $('#previous').click('click',function(e) {
     }
     $('#newsArticle-'+article_index).fadeIn(2000);
 });
+//function that loads the last viewed article from local storage
 function loadSettings () {
-     console.log(localStorage.articleNumber);
     if(localStorage.articleNumber === NaN) {
         article_index = -1;
     }
@@ -99,8 +112,8 @@ function loadSettings () {
         article_index = localStorage.articleNumber;
     }
 }
+//function that saves the last seen article to local storage
 function saveSettings() {
-    console.log(article_index)
     if(localStorage.articleNumber !== NaN) {
         localStorage.articleNumber = article_index - 1;
     }
