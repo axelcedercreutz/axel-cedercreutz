b39393c5754c59402189b648bc6c1d908f44475b
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-03-20 10:51:42 +0200

    trying to fix canvas

--- js/app.js
+++ js/app.js
@@ -37,6 +37,9 @@ var aChildren = $(".navbar-right li").children(); // find the a children of the 
         }
     }
 });
+var drawStop = false
+var drawNega = false
+var drawBlack = false
 document.addEventListener('DOMContentLoaded', function(){
     var v = document.getElementById('v');
     var canvas = document.getElementById('c');
@@ -48,18 +51,55 @@ document.addEventListener('DOMContentLoaded', function(){
 
     v.addEventListener('play', function(){
         cw = v.clientWidth;
+        console.log(cw);
         ch = v.clientHeight;
+        console.log(ch);
         canvas.width = cw;
         canvas.height = ch;
         back.width = cw;
         back.height = ch;
         draw(v,context,backcontext,cw,ch);
     },false);
-
+    $('#normal').click('click', function(e) {
+        drawStop = false;
+        drawNega = true;
+        drawBlack = true;
+        draw(v,context,backcontext,cw,ch);
+    });
+    $('#black-white').click('click', function(e) {
+            drawStop = true;
+            drawNega = true;
+            drawBlack = false;
+            drawBlackWhite(v,context,backcontext,cw,ch);
+    });
+    $('#negative').click('click', function(e) {
+            drawStop = true;
+            drawBlack = true;
+            drawNega = false;
+            drawNegative(v,context,backcontext,cw,ch);
+    });
 },false);
-$('button.negative').click('click', function(e) {
 function draw(v,c,bc,cw,ch) {
-    if(v.paused || v.ended) return false;
+    if(v.paused || v.ended || drawStop) return false;
+    // First, draw it into the backing canvas
+    bc.drawImage(v,0,0,cw,ch);
+    // Grab the pixel data from the backing canvas
+    var idata = bc.getImageData(0,0,cw,ch);
+    var data = idata.data;
+    var w = idata.width;
+    var limit = data.length
+    // Loop through the subpixels, convoluting each using an edge-detection matrix.
+    // for(var i = 0; i < limit; i++) {
+    //     if( i%4 == 3 ) continue;
+    //     data[i] = 127 + 2*data[i] - data[i + 4] - data[i + w*4];
+    // }
+    // Draw the pixels onto the visible canvas
+    c.putImageData(idata,0,0);
+    // Start over!
+    setTimeout(draw,20,v,c,bc,cw,ch);
+ }
+function drawNegative(v,c,bc,cw,ch) {
+    if(v.paused || v.ended || drawNega) return false;
     // First, draw it into the backing canvas
     bc.drawImage(v,0,0,cw,ch);
     // Grab the pixel data from the backing canvas
@@ -75,33 +115,30 @@ function draw(v,c,bc,cw,ch) {
     // Draw the pixels onto the visible canvas
     c.putImageData(idata,0,0);
     // Start over!
-    setTimeout(draw,20,v,c,bc,cw,ch);
+    setTimeout(function() { drawNegative(v,c,bc,cw,ch); }, 0)
+ }
+function drawBlackWhite(v,c,bc,w,h) {
+    console.log("testing");
+    if(v.paused || v.ended || drawBlack) return false;
+    // First, draw it into the backing canvas
+    bc.drawImage(v,0,0,w,h);
+    // Grab the pixel data from the backing canvas
+    var idata = bc.getImageData(0,0,w,h);
+    var data = idata.data;
+    console.log(data);
+    // Loop through the pixels, turning them grayscale
+    for(var i = 0; i < data.length; i+=4) {
+        var r = data[i];
+        var g = data[i+1];
+        var b = data[i+2];
+        var brightness = (3*r+4*g+b)>>>3;
+        data[i] = brightness;
+        data[i+1] = brightness;
+        data[i+2] = brightness;
     }
-});
-
-$('button.black-white').click('click', function(e) {
-    function draw(v,c,bc,w,h) {
-        console.log("testing");
-        if(v.paused || v.ended) return false;
-        // First, draw it into the backing canvas
-        bc.drawImage(v,0,0,w,h);
-        // Grab the pixel data from the backing canvas
-        var idata = bc.getImageData(0,0,w,h);
-        var data = idata.data;
-        // Loop through the pixels, turning them grayscale
-        for(var i = 0; i < data.length; i+=4) {
-            var r = data[i];
-            var g = data[i+1];
-            var b = data[i+2];
-            var brightness = (3*r+4*g+b)>>>3;
-            data[i] = brightness;
-            data[i+1] = brightness;
-            data[i+2] = brightness;
-        }
-        idata.data = data;
-        // Draw the pixels onto the visible canvas
-        c.putImageData(idata,0,0);
-        // Start over!
-        setTimeout(function(){ draw(v,c,bc,w,h); }, 0);
-    }
-});
+    idata.data = data;
+    // Draw the pixels onto the visible canvas
+    c.putImageData(idata,0,0);
+    // Start over!
+    setTimeout(function(){ drawBlackWhite(v,c,bc,w,h); }, 0);
+}
